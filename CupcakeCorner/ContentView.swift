e3e2ee0f2d931b111b140414e7d811e60f9332f0
Daniil Kukhar <82108121+DaniilKukhar@users.noreply.github.com>
2023-01-26 17:19:03 +0200

    Add files to the project

---
+++ CupcakeCorner/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Danya Kukhar on 26.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
